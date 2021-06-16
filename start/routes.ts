a7b8b8f59eb42d42efa09518b644496715a0b0a2
Henri Briaux <henri.briaux@viacesi.fr>
2021-06-17 00:12:02 +0200

    token generate

--- start/routes.ts
+++ start/routes.ts
@@ -1,15 +1,25 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
 
-Route.get('posts', async () => {
-  return Database.from('User').select('*')
+import Hash from '@ioc:Adonis/Core/Hash'
+
+
+Route.post('posts', async ({auth, request, response}) => {
+
+
+  const email = request.input('email')
+  const password = request.input('password')
+
+  // Lookup user manually
+  const user = await User.findByOrFail('email','Henri')
+
+  // Generate token
+  const token = await auth.use('api').generate(user)
+
+  return token
 })
 
 Route.get('get', async () => {
-  return await Database
-  .table('User')
-  .insert({
-    name: 'Adoo',
-    password: 'Loo'
-  })
+  return await User.create({ email: 'Henri', password:'youpi' })
 })
