04c5a2a325a7e5931d70a5683ea0d585f9e43eef
Henri Briaux <henri.briaux@viacesi.fr>
2021-06-16 16:30:49 +0200

    first commit

---
+++ providers/MongoProvider.ts
@@ -0,0 +1,61 @@
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+import { Mongoose } from 'mongoose'
+
+/*
+|--------------------------------------------------------------------------
+| Provider
+|--------------------------------------------------------------------------
+|
+| Your application is not ready when this file is loaded by the framework.
+| Hence, the top level imports relying on the IoC container will not work.
+| You must import them inside the life-cycle methods defined inside
+| the provider class.
+|
+| @example:
+|
+| public async ready () {
+|   const Database = this.app.container.resolveBinding('Adonis/Lucid/Database')
+|   const Event = this.app.container.resolveBinding('Adonis/Core/Event')
+|   Event.on('db:query', Database.prettyPrint)
+| }
+|
+*/
+export default class MongoProvider {
+  constructor (protected app: ApplicationContract) {
+  }
+
+  public register () {
+    // Register your own bindings
+    const mongoose = new Mongoose()
+
+    /*
+    mongoose.connect('mongodb+srv://Geralt:Ciri@'+
+    'corbeilleexo.tag2v.mongodb.net:/A4L',  {useNewUrlParser: true, useUnifiedTopology: true})
+    */
+
+    mongoose.connect('mongodb+srv://corbeilleexo.tag2v.mongodb.net:/A4L?authSource=%24external&authMechanism=MONGODB-X509', {
+      useUnifiedTopology: true,
+      ssl: true,
+      sslCert: require('fs').readFileSync(`C:/Users/henri/Desktop/WorshopWeb/Corbeille3/X509-cert-3830741774116890921.pem`),
+      sslKey:require('fs').readFileSync(`C:/Users/henri/Desktop/WorshopWeb/Corbeille3/X509-cert-3830741774116890921.pem`),
+
+    });
+
+    // Lorsqu'on fait un import donne l'accès à la BDD
+    this.app.container.singleton('Mongoose', ()=> mongoose)
+
+  }
+
+  public async boot () {
+    // All bindings are ready, feel free to use them
+  }
+
+  public async ready () {
+    // App is ready
+  }
+
+  public async shutdown () {
+    // Cleanup, since app is going down
+    await this.app.container.use('Mongoose').disconnect()
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,15 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('posts', async () => {
+  return Database.from('User').select('*')
+})
+
+Route.get('get', async () => {
+  return await Database
+  .table('User')
+  .insert({
+    name: 'Ado01',
+    password: 'Loo'
+  })
+})
