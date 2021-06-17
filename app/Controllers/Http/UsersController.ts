9599522cb0b1e5131d012cc52300ffff2e06ce31
Henri Briaux <henri.briaux@viacesi.fr>
2021-06-17 16:40:58 +0200

    Token handled pt2

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,4 +1,9 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import User from 'App/Models/User'
 export default class UsersController {
+
+    public async create(){       
+        return await User.create({ email: 'Henri', password:'youpi' })
+    }
+    
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,51 +1,12 @@
-import Database from '@ioc:Adonis/Lucid/Database'
 import Route from '@ioc:Adonis/Core/Route'
-import User from 'App/Models/User'
-import Hash from '@ioc:Adonis/Core/Hash'
-import jwt from 'jsonwebtoken'
 
 
 
-Route.get('signup', async () => {
-  return await User.create({ email: 'Henri', password:'youpi' })
-})
 
-Route.post('login', async ({ auth, request, response }) => {
-  // Get the param of user in the request
-  const email = request.input('email')
-  const password = request.input('password')
-  try {
+Route.post('signup', 'UserController.create')
 
-    // Generate token
-    const token =  jwt.sign({
-      data: 'foobar'
-    }, 'TOKEN_PRIVATE_KEY', { expiresIn: '30m' });
+Route.post('login','AuthController.login')
 
-    // Generate refresh token
-    const refresh_token = await auth.use('api').attempt(email, password, {
-      expiresIn:'1days'
-    })
+Route.get('refresh_token', 'AuthController.refresh_token')
 
-    // Give the token to the client
-    return {token, refresh_token}
-
-  } catch {
-    return response.badRequest('Invalid credentials')
-  }
-})
-
-Route.get('refresh_token', async({auth,  response})=>{
-  try {
-    // Check refresh token and give an acces token
-    const refreshToken = await auth.use('api').authenticate()
-
-    if (refreshToken) return jwt.sign({ data: 'foobar' }, 'TOKEN_PRIVATE_KEY', { expiresIn: '30m' });
-  
-  }catch{
-    return response.badRequest('Invalid credentials')
-  }
-})
-
-Route.get('check', async({request})=>{
-  return jwt.verify(request.input('token'), 'TOKEN_PRIVATE_KEY');
-})
+Route.get('check', 'AuthController.check')
