1308eae37f4544878c10ab1682e4290b916cb44c
Sebastien-Bardin <33619819+Sebastien-Bardin@users.noreply.github.com>
2021-06-24 15:19:21 +0200

    modification middleware
    ajout d'un retour de l'erreur plus précis

--- app/Middleware/CheckJwt.ts
+++ app/Middleware/CheckJwt.ts
@@ -5,7 +5,7 @@ export default class CheckJwt {
 
   public async handle ({request, response}: HttpContextContract, next: () => Promise<void>) {
     await jwt.verify(request.input('jwt'), "TOKEN_PRIVATE_KEY", function(err) {
-      if(err)return response.unauthorized("JWT Token error")
+      if(err)return response.unauthorized({message:"JWT Token error", error : err})
       next()
     });
 
