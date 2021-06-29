06a317ed7cd62a455c1f1b2dd71a065907017f0f
Henri Briaux <henri.briaux@viacesi.fr>
2021-06-29 17:00:32 +0200

    bug cors correction

--- build/start/routes.js
+++ build/start/routes.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
-Route_1.default.get('test', () => {
-    return 'PAGE DE TEST';
+Route_1.default.get('', () => {
+    return '';
 });
 Route_1.default.post('login_auth', 'AuthController.loginAuth');
 Route_1.default.get('refresh_token_auth', 'AuthController.refreshTokenAuth');
