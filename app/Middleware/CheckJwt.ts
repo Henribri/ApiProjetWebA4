d7e72c4256364d2a96fe240bcde4058af4ba2c9e
Henri Briaux <henri.briaux@viacesi.fr>
2021-06-30 15:15:56 +0200

    check jwt with authorization header

--- app/Middleware/CheckJwt.ts
+++ app/Middleware/CheckJwt.ts
@@ -1,10 +1,10 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import jwt from 'jsonwebtoken'
 
 export default class CheckJwt {
 
-  public async handle ({request, response}: HttpContextContract, next: () => Promise<void>) {
-    await jwt.verify(request.input('jwt'), "TOKEN_PRIVATE_KEY", function(err) {
+  public async handle ({request, response}, next: () => Promise<void>) {
+    const token = request.header('authorization').split(" ")
+    await jwt.verify(token[1], "TOKEN_PRIVATE_KEY", function(err) {
       if(err)return response.unauthorized({message:"JWT Token error", error : err})
       next()
     });
